c13763de54267138e5e5fb93371f63eb3426f379
dcwik96 <greek96@interia.pl>
2018-12-09 17:49:22 +0100

    migracja z java 8 do 11

--- server/src/main/java/pl/smarthome/smarthome/SmarthomeApplication.java
+++ server/src/main/java/pl/smarthome/smarthome/SmarthomeApplication.java
@@ -1,4 +1,4 @@
-package com.project.smarthome;
+package pl.smarthome.smarthome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
