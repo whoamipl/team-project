be77b74afc7e764b220af573574cf92f755cd9a3
olekkosma <kosma.aleksander@gmail.com>
2019-01-13 17:46:03 +0100

    added request to mocked sonoff from app

--- app/app/src/main/java/com/home/zmart/zmarthome/MainActivity.kt
+++ app/app/src/main/java/com/home/zmart/zmarthome/MainActivity.kt
@@ -1,50 +1,61 @@
 package com.home.zmart.zmarthome
 
-import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import com.android.volley.AuthFailureError
+import android.util.Base64.NO_WRAP
+import com.android.volley.VolleyError
 import org.json.JSONArray
-import java.net.HttpURLConnection
-import java.net.URL
+import org.json.JSONObject
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.RetryPolicy
+
+
+
 
 class MainActivity : AppCompatActivity() {
+//    val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val url = "https://mysafeinfo.com/api/data?list=presidents&format=json"
+        val url = "http://192.168.0.15:8081/"
+        val queue = Volley.newRequestQueue(this)
 
-        AsyncTaskHandleJson().execute(url)
-    }
-    inner class AsyncTaskHandleJson : AsyncTask<String,String,String>(){
-        override fun doInBackground(vararg url: String?): String {
-            var text: String
-            val connection = URL(url[0]).openConnection() as HttpURLConnection
-            try{
-                connection.connect()
-                text = connection.inputStream.use{it.reader().use{reader -> reader.readText()}}
+        button.setOnClickListener {
+            val stringRequest = object: StringRequest(Request.Method.GET, url,
+                    Response.Listener { response ->
 
-            }finally {
-                connection.disconnect()
+                        Log.d("A", "Response is: " + response)
+                        var strResp = response.toString()
+                        val jsonObj: JSONObject = JSONObject(strResp)
+                        var res = jsonObj.get("status")
+                        textView_status.text=res.toString()
+                    },
+                    Response.ErrorListener {error ->
+
+                        textView_status.text = "ERROR: %s".format(error.toString())
+                    })
+            {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers = HashMap<String, String>()
+                    val credentials = "TheBestTeam:WiesioKiller"
+                    val auth = "Basic " + Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
+                    headers.put("Content-Type", "application/json")
+                    headers.put("Authorization", auth)
+                    return headers
+                }
             }
-            return  text
+            queue.add(stringRequest)
         }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            handleJson(result)
-        }
-        
-
     }
-
-    private fun handleJson(jsonString: String?) {
-
-        val jsonArray = JSONArray(jsonString)
-
-        textView_status.text = jsonArray.get(0).toString()
-    }
-
 }
