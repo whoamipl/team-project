ab8c94649e7e72ebe00dd5926d0b53f421b65e54
Michał Fierek <mfierek@protonmail.ch>
2019-01-13 21:20:18 +0100

    Corrected content header, disable auth on status

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,8 +4,8 @@
 #include <ESP8266mDNS.h>
 
 // This header is not attached to repo, also is added to .gitgnore.
-// Setup it with your web credentials.
-#include "credentials.h"
+// Setup it with your web credentials.QQQQQQ
+#include "credentials_shema.h"
 MDNSResponder mdns;
 ESP8266WebServer server(80);
 
@@ -18,15 +18,15 @@ char* www_password = "WiesioKiller";
 int gpio_13_led = 13;
 int gpio_12_relay = 12;
 
-void setup(void){  
+void setup(void) {
   //  Init
   pinMode(gpio_13_led, OUTPUT);
   digitalWrite(gpio_13_led, HIGH);
-  
+
   pinMode(gpio_12_relay, OUTPUT);
   digitalWrite(gpio_12_relay, HIGH);
- 
-  Serial.begin(115200); 
+
+  Serial.begin(115200);
   delay(5000);
 
   WiFi.begin(ssid, password);
@@ -37,52 +37,49 @@ void setup(void){
     delay(1000);
     ESP.restart();
   }
-  
+
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  
+
   if (mdns.begin("esp8266", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
-  
-  server.on("/", [](){
-    if (!server.authenticate(www_username, www_password)) {
-      return server.requestAuthentication();
-    }
-    if(digitalRead(gpio_12_relay)==HIGH) {
-      server.send(200, "text/html", web_on_html);
+
+  server.on("/", []() {
+    if (digitalRead(gpio_12_relay) == HIGH) {
+      server.send(200, "application/json", web_on_html);
     } else {
-      server.send(200, "text/html", web_off_html);
+      server.send(200, "application/json", web_off_html);
     }
   });
-  
-  server.on("/on", [](){
+
+  server.on("/on", []() {
     if (!server.authenticate(www_username, www_password)) {
       return server.requestAuthentication();
     }
-    server.send(200, "text/html", web_on_html);
+    server.send(200, "application/json", web_on_html);
     digitalWrite(gpio_13_led, LOW);
     digitalWrite(gpio_12_relay, HIGH);
     delay(1000);
   });
-  
-  server.on("/off", [](){
+
+  server.on("/off", []() {
     if (!server.authenticate(www_username, www_password)) {
       return server.requestAuthentication();
     }
-    server.send(200, "text/html", web_off_html);
+    server.send(200, "application/json", web_off_html);
     digitalWrite(gpio_13_led, HIGH);
     digitalWrite(gpio_12_relay, LOW);
-    delay(1000); 
+    delay(1000);
   });
-  
+
   server.begin();
   Serial.println("Server ready..");
 }
- 
-void loop(void){
+
+void loop(void) {
   server.handleClient();
 }
