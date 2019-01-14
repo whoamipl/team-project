4596a230fdf64605413455e7970a88e5eaf6bb8e
olekkosma <kosma.aleksander@gmail.com>
2019-01-14 23:33:10 +0100

    added fully working end points toggle and status

--- server/src/main/java/pl/smarthome/smarthome/SmarthomeController.java
+++ server/src/main/java/pl/smarthome/smarthome/SmarthomeController.java
@@ -1,82 +1,63 @@
 package pl.smarthome.smarthome;
 
-import jdk.incubator.http.HttpClient;
-import jdk.incubator.http.HttpRequest;
-import jdk.incubator.http.HttpResponse;
-import org.apache.catalina.authenticator.BasicAuthenticator;
-import org.springframework.util.Base64Utils;
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @RestController
 public class SmarthomeController {
-    private final static String baseSwitchUrl = "http://192.168.0.100";
+    private final static String baseSwitchUrl = "http://192.168.0.15:8081";
     private final static String switchCredentials = "TheBestTeam:WiesioKiller";
-    private static SwitchStatus status;
+    private static SwitchStatus status = new SwitchStatus();
+    String encodedCredentials = Base64.getEncoder().encodeToString(switchCredentials.getBytes(StandardCharsets.UTF_8));
 
     @GetMapping("/toggle")
-    public void changeSwitchStatus() {
-        // Determine how to get switch status
-        // Temporary set initial status to on
-        status.setStatus("on");
-        var httpClient = HttpClient.newHttpClient();
-
-        var encodedCredentials = Base64.getEncoder().encode(switchCredentials.getBytes());
-        var requestBuilder = HttpRequest.newBuilder();
-        HttpResponse response;
+    public String changeSwitchStatus() throws IOException {
+        status.setStatus(getStatus("/status"));
 
         if (status.getStatus().equals("off")) {
-            try {
-                response = httpClient.send(
-                        requestBuilder
-                                .GET()
-                                .header("Authorization", "Basic" + encodedCredentials)
-                                .uri(URI.create(baseSwitchUrl + "/on"))
-                                .build(),
-                        HttpResponse.BodyHandler.asString()
-                );
-            }
-            catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            try {
-                response = httpClient.send(
-                        requestBuilder
-                                .GET()
-                                .header("Authorization", "Basic" + encodedCredentials)
-                                .uri(URI.create(baseSwitchUrl + "/off"))
-                                .build(),
-                        HttpResponse.BodyHandler.asString()
-                );
-            }
-            catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
+            return getStatus("/on");
+
+        } else {
+            return getStatus("/off");
         }
     }
 
     @GetMapping("/status")
-    public String getSwitchStatus() {
-        // We get status from switch and sent it back.
-        var request = HttpRequest.newBuilder(URI.create(baseSwitchUrl)).build();
-        var client = HttpClient.newHttpClient();
-        HttpResponse<String> response = null;
+    public String getSwitchStatus() throws IOException {
 
-        try {
-            response = client.send(request, HttpResponse.BodyHandler.asString());
-        }
-        catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        return response.body();
+        return getStatus("/status");
     }
 
+    private String getStatus(String s) throws IOException {
+        URL url = new URL(baseSwitchUrl + s);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("Authorization", "Basic " + encodedCredentials);
+        con.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.println(response);
+        Gson g = new Gson();
+        SwitchStatus p = g.fromJson(String.valueOf(response), SwitchStatus.class);
+        return p.getStatus();
+    }
 }
