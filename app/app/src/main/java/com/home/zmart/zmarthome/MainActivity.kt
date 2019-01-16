7ba8e32149f87864f5ddc5b0577bb7b3626e22b2
olekkosma <kosma.aleksander@gmail.com>
2019-01-16 13:22:22 +0100

    added sending request about status per second full connection between fake.sonof  - backend - frontend

--- app/app/src/main/java/com/home/zmart/zmarthome/MainActivity.kt
+++ app/app/src/main/java/com/home/zmart/zmarthome/MainActivity.kt
@@ -2,60 +2,78 @@ package com.home.zmart.zmarthome
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Base64
 import android.util.Log
-import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import com.android.volley.AuthFailureError
 import android.util.Base64.NO_WRAP
-import com.android.volley.VolleyError
+import com.android.volley.*
+import org.jetbrains.anko.toast
 import org.json.JSONArray
 import org.json.JSONObject
-import com.android.volley.DefaultRetryPolicy
-import com.android.volley.RetryPolicy
-
-
 
 
 class MainActivity : AppCompatActivity() {
 //    val client = OkHttpClient()
+    private var handler: Handler? = null
+    val url = "http://192.168.0.15:8082/status"
+    var queue: RequestQueue? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        queue = Volley.newRequestQueue(this)
+        handler = Handler()
 
-        val url = "http://192.168.0.15:8081/"
-        val queue = Volley.newRequestQueue(this)
+        handler!!.postDelayed(makeToast, 1500)
 
-        button.setOnClickListener {
-            val stringRequest = object: StringRequest(Request.Method.GET, url,
-                    Response.Listener { response ->
 
-                        Log.d("A", "Response is: " + response)
-                        var strResp = response.toString()
-                        val jsonObj: JSONObject = JSONObject(strResp)
-                        var res = jsonObj.get("status")
-                        textView_status.text=res.toString()
-                    },
-                    Response.ErrorListener {error ->
+        setButtonListener(url)
+    }
 
-                        textView_status.text = "ERROR: %s".format(error.toString())
-                    })
-            {
-                @Throws(AuthFailureError::class)
-                override fun getHeaders(): Map<String, String> {
-                    val headers = HashMap<String, String>()
-                    val credentials = "TheBestTeam:WiesioKiller"
-                    val auth = "Basic " + Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
-                    headers.put("Content-Type", "application/json")
-                    headers.put("Authorization", auth)
-                    return headers
-                }
-            }
-            queue.add(stringRequest)
+    private val makeToast = object : Runnable {
+        override fun run(){
+            toast("Test toast")
+            sendRequestAboutStatus(url)
+            handler!!.postDelayed(this, 1000)
+
         }
+    }
+
+
+
+    private fun setButtonListener(url: String) {
+        button.setOnClickListener {
+            sendRequestAboutStatus(url)
+        }
+    }
+
+    private fun sendRequestAboutStatus(url: String) {
+        val stringRequest = object : StringRequest(Method.GET, url,
+                Response.Listener { response ->
+
+                    Log.d("A", "Response is: " + response)
+                    var strResp = response.toString()
+                   // val jsonObj: JSONObject = JSONObject(strResp)
+                    //var res = jsonObj.get("status")
+                    textView_status.text = strResp
+                },
+                Response.ErrorListener { error ->
+
+                    textView_status.text = "ERROR: %s".format(error.toString())
+                }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                val credentials = "TheBestTeam:WiesioKiller"
+                val auth = "Basic " + Base64.encodeToString(credentials.toByteArray(), NO_WRAP)
+                headers.put("Content-Type", "application/json")
+                headers.put("Authorization", auth)
+                return headers
+            }
+        }
+        queue!!.add(stringRequest)
     }
 }
