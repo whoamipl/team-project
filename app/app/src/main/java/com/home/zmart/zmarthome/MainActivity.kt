9cd05a990e161d1a872bdb2920f30d57c241bb45
olekkosma <kosma.aleksander@gmail.com>
2019-01-16 20:02:58 +0100

    changed UI, refactor of code

--- app/app/src/main/java/com/home/zmart/zmarthome/MainActivity.kt
+++ app/app/src/main/java/com/home/zmart/zmarthome/MainActivity.kt
@@ -3,7 +3,6 @@ package com.home.zmart.zmarthome
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.SystemClock
 import android.util.Base64
 import android.util.Log
 import com.android.volley.toolbox.StringRequest
@@ -11,10 +10,11 @@ import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import android.util.Base64.NO_WRAP
 import com.android.volley.*
+import android.graphics.drawable.TransitionDrawable
+import android.graphics.drawable.ColorDrawable
 
 
 class MainActivity : AppCompatActivity() {
-    //    val client = OkHttpClient()
     private var handler: Handler? = null
     val url = "http://192.168.0.15:8082"
     val username = "TheBestTeam"
@@ -22,12 +22,12 @@ class MainActivity : AppCompatActivity() {
     var queue: RequestQueue? = null
     var statusValue = ""
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         queue = Volley.newRequestQueue(this)
         handler = Handler()
-
-        handler!!.postDelayed(checkStatus, 1800)
+        handler!!.postDelayed(checkStatus, 0)
 
 
         setButtonListener(url)
@@ -36,27 +36,22 @@ class MainActivity : AppCompatActivity() {
     private val checkStatus = object : Runnable {
         override fun run() {
             sendRequestAboutStatus(url + "/status")
-            handler!!.postDelayed(this, 1000)
+            handler!!.postDelayed(this, 1500)
 
         }
     }
 
     private fun setButtonListener(url: String) {
         button.setOnClickListener {
-            setStatus()
-
             toggle(url + "/toggle")
         }
     }
 
     private fun toggle(url: String) {
-
-
         val stringRequest = object : StringRequest(Method.GET, url,
                 Response.Listener { response ->
                     logResponse(response)
                     statusValue = response.toString()
-                    //setStatus()
                 },
 
                 Response.ErrorListener { error ->
@@ -84,6 +79,8 @@ class MainActivity : AppCompatActivity() {
         val stringRequest = object : StringRequest(Method.GET, url,
                 Response.Listener { response ->
                     logResponse(response)
+
+                    makePulseEffect()
                     statusValue = response.toString()
                     setStatus()
                 },
@@ -100,6 +97,18 @@ class MainActivity : AppCompatActivity() {
         queue!!.add(stringRequest)
     }
 
+    private fun makePulseEffect() {
+        pulse(this.getColor(R.color.grey), this.getColor(R.color.white))
+        pulse(this.getColor(R.color.white), this.getColor(R.color.grey))
+    }
+
+    private fun pulse(color1: Int,color2: Int) {
+        var color = arrayOf(ColorDrawable(color1), ColorDrawable(color2))
+        var trans = TransitionDrawable(color)
+        layout.setBackground(trans)
+        trans.startTransition(300)
+    }
+
     private fun logResponse(response: String?) {
         Log.d("RESPONSE", "Response is: " + response)
     }
@@ -112,8 +121,6 @@ class MainActivity : AppCompatActivity() {
         } else {
             textView_status.text = statusValue
             button.setImageResource(R.drawable.button_on)
-
         }
     }
-
 }
